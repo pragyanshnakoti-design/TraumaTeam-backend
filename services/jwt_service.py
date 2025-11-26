249b43c1d9af75d93c270fe99c22081ee36020f0
pragyanshnakoti-design <pragyanshnakoti@gmail.com>
2025-11-26 08:49:55 +0530

    Create jwt_service.py

---
+++ services/jwt_service.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = "supersecretkey_traumateam_backend"  # change if you want, but keep same in backend
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  # 24 hours
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except Exception:
+        return None
